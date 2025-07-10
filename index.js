3d0a008bd1b3ff61484a52505091a0ca7107f33a
Deniz <auo@tuta.io>
2025-07-10 20:22:11 +0200

    added logic added styles

--- index.js
+++ index.js
@@ -31,23 +31,56 @@ function handleSearchBtn(e) {
                             const moviePoster = document.createElement("img");
                             moviePoster.src = movie.Poster;
                             moviePoster.className = "movie-poster";
+                    
+                            const movieContainer = document.createElement("div");
 
-                            const movieNameContainer = document.createElement("div");
-                            movieNameContainer.className = "movie-name-container";
+                                const movieNameContainer = document.createElement("div");
+                                movieNameContainer.className = "movie-info-container";
 
 
-                                const movieTitle = document.createElement("h3");
-                                movieTitle.textContent = movie.Title;
+                                    const movieTitle = document.createElement("h3");
+                                    movieTitle.textContent = movie.Title;
 
-                                const starIcon = document.createElement("img");
-                                starIcon.src = "./media/star-icon.png";
+                                    const starIcon = document.createElement("img");
+                                    starIcon.src = "./media/star-icon.png";
 
-                                const ratingScore = document.createElement("p");
-                                ratingScore.textContent = movie.imdbRating;
+                                    const ratingScore = document.createElement("p");
+                                    ratingScore.textContent = movie.imdbRating;
 
-                            movieNameContainer.append(movieTitle, starIcon, ratingScore);
+                                movieNameContainer.append(movieTitle, starIcon, ratingScore);
+                    
+                                const detailsContainer = document.createElement("div");
+                                detailsContainer.className = "movie-info-container";
+                    
+                                    const lengthPara = document.createElement("p");
+                                    lengthPara.textContent = movie.Runtime;
+                    
+                                    const genresPara = document.createElement("p");
+                                    genresPara.textContent = movie.Genre;
+                    
+                                    const addButton = document.createElement("button");
+                    
+                                        const addIcon = document.createElement("img");
+                                        addIcon.src = "./media/plus.png";
+                    
+                                        const addPara = document.createElement("p");
+                                        addPara.textContent = "Watchlist";
+                                       
+                    
+                                    addButton.append(addIcon, addPara);
+                    
+                    
+                    
+                                detailsContainer.append(lengthPara, genresPara, addButton);
+                    
+                                const resumePara = document.createElement("p");
+                                resumePara.textContent = movie.Plot;
+                    
+        
+                    
+                            movieContainer.append(movieNameContainer, detailsContainer, resumePara);
 
-                        div.append(moviePoster, movieNameContainer);
+                        div.append(moviePoster, movieContainer);
                     
                     resultsContainer.append(div);
                     
