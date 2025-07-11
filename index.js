b7c9ca622daf936966d49049265df9b9b94d8ed4
Deniz <auo@tuta.io>
2025-07-11 17:19:44 +0200

    perfected grid layout added logic to read more

--- index.js
+++ index.js
@@ -10,15 +10,13 @@ document.addEventListener("click", function (e) {
 function handleSearchBtn(e) {
     e.preventDefault();
     resultsContainer.innerHTML = "";
-    
-    
 
     fetch(`https://www.omdbapi.com/?apikey=73de4715&s=${searchInput.value}`)
         .then((res) => res.json())
         .then((data) => {
             //console.log(data);
 
-            data.Search.forEach((movie) => {
+            data.Search.forEach((movie, i) => {
                 //console.log(movie.Title);
 
                 fetch(`https://www.omdbapi.com/?apikey=73de4715&i=${movie.imdbID}`)
@@ -26,70 +24,49 @@ function handleSearchBtn(e) {
                     .then((movie) => {
                         console.log(movie);
                     
-                        //const div = document.createElement("div");
-//
+                    
+                        //const gridItem = document.createElement("div");
+                        //gridItem.className = "grid-item poster-box";
+                    //
                         //    const moviePoster = document.createElement("img");
                         //    moviePoster.src = movie.Poster;
-                        //    moviePoster.className = "movie-poster";
                     //
-                        //    const movieContainer = document.createElement("div");
-//
-                        //        const movieNameContainer = document.createElement("div");
-                        //        movieNameContainer.className = "movie-info-container";
-//
-//
-                        //            const movieTitle = document.createElement("h3");
-                        //            movieTitle.textContent = movie.Title;
-//
-                        //            const starIcon = document.createElement("img");
-                        //            starIcon.src = "./media/star-icon.png";
-//
-                        //            const ratingScore = document.createElement("p");
-                        //            ratingScore.textContent = movie.imdbRating;
-//
-                        //        movieNameContainer.append(movieTitle, starIcon, ratingScore);
-                    //
-                        //        const detailsContainer = document.createElement("div");
-                        //        detailsContainer.className = "movie-info-container";
-                    //
-                        //            const lengthPara = document.createElement("p");
-                        //            lengthPara.textContent = movie.Runtime;
-                    //
-                        //            const genresPara = document.createElement("p");
-                        //            genresPara.textContent = movie.Genre;
-                    //
-                        //            const addButton = document.createElement("button");
-                    //
-                        //                const addIcon = document.createElement("img");
-                        //                addIcon.src = "./media/plus.png";
-                    //
-                        //                const addPara = document.createElement("p");
-                        //                addPara.textContent = "Watchlist";
-                        //               
-                    //
-                        //            addButton.append(addIcon, addPara);
+                        //gridItem.append(moviePoster);
                     //
                     //
-                    //
-                        //        detailsContainer.append(lengthPara, genresPara, addButton);
-                    //
-                        //        const resumePara = document.createElement("p");
-                        //        resumePara.textContent = movie.Plot;
-                        //    
-                    //
-        //
-                    //
-                        //    movieContainer.append(movieNameContainer, detailsContainer, resumePara);
-//
-                        //div.append(moviePoster, movieContainer);
-                    //
-                    //resultsContainer.append(div);
-                    
+                        //resultsContainer.append(gridItem);
+
+                        resultsContainer.innerHTML += `
+                        <div class="movie-container" id="movie-container-${i}">
+                            <div class="grid-item poster-box">
+                                <img src="${movie.Poster}"/>
+                            </div>
+                            <div class="grid-item name-box">
+                                <h3>${movie.Title} </h3>
+                                <img src="media/star-icon.png" />
+                                <p>${movie.imdbRating}</p>
+                            </div>
+                            <div class="grid-item details-box">
+                                <p>${movie.Runtime}</p>
+                                <p>${movie.Genre}</p>
+                                <button id="add-watch-btn-${i}">
+                                    <img src="media/plus.png" />
+                                    Watchlist
+                                </button>
+                            </div>
+                            <div class="grid-item theme-box">
+                                <p>
+                                    ${movie.Plot}
+                                </p>
+                            </div>
+                        </div>
+                        `;
+
                     });
             });
         });
-    
+
     /*Reset form field*/
-    
+
     document.getElementById("search-form").reset();
 }
