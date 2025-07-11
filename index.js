ae5557995e756de139ce827a2c3f47db8df86e6c
Deniz <auo@tuta.io>
2025-07-11 11:24:44 +0200

    Changed the layout logic to grid

--- index.js
+++ index.js
@@ -26,64 +26,64 @@ function handleSearchBtn(e) {
                     .then((movie) => {
                         console.log(movie);
                     
-                        const div = document.createElement("div");
-
-                            const moviePoster = document.createElement("img");
-                            moviePoster.src = movie.Poster;
-                            moviePoster.className = "movie-poster";
-                    
-                            const movieContainer = document.createElement("div");
-
-                                const movieNameContainer = document.createElement("div");
-                                movieNameContainer.className = "movie-info-container";
-
-
-                                    const movieTitle = document.createElement("h3");
-                                    movieTitle.textContent = movie.Title;
-
-                                    const starIcon = document.createElement("img");
-                                    starIcon.src = "./media/star-icon.png";
-
-                                    const ratingScore = document.createElement("p");
-                                    ratingScore.textContent = movie.imdbRating;
-
-                                movieNameContainer.append(movieTitle, starIcon, ratingScore);
-                    
-                                const detailsContainer = document.createElement("div");
-                                detailsContainer.className = "movie-info-container";
-                    
-                                    const lengthPara = document.createElement("p");
-                                    lengthPara.textContent = movie.Runtime;
-                    
-                                    const genresPara = document.createElement("p");
-                                    genresPara.textContent = movie.Genre;
-                    
-                                    const addButton = document.createElement("button");
-                    
-                                        const addIcon = document.createElement("img");
-                                        addIcon.src = "./media/plus.png";
-                    
-                                        const addPara = document.createElement("p");
-                                        addPara.textContent = "Watchlist";
-                                       
-                    
-                                    addButton.append(addIcon, addPara);
-                    
-                    
-                    
-                                detailsContainer.append(lengthPara, genresPara, addButton);
-                    
-                                const resumePara = document.createElement("p");
-                                resumePara.textContent = movie.Plot;
-                            
-                    
-        
-                    
-                            movieContainer.append(movieNameContainer, detailsContainer, resumePara);
-
-                        div.append(moviePoster, movieContainer);
-                    
-                    resultsContainer.append(div);
+                        //const div = document.createElement("div");
+//
+                        //    const moviePoster = document.createElement("img");
+                        //    moviePoster.src = movie.Poster;
+                        //    moviePoster.className = "movie-poster";
+                    //
+                        //    const movieContainer = document.createElement("div");
+//
+                        //        const movieNameContainer = document.createElement("div");
+                        //        movieNameContainer.className = "movie-info-container";
+//
+//
+                        //            const movieTitle = document.createElement("h3");
+                        //            movieTitle.textContent = movie.Title;
+//
+                        //            const starIcon = document.createElement("img");
+                        //            starIcon.src = "./media/star-icon.png";
+//
+                        //            const ratingScore = document.createElement("p");
+                        //            ratingScore.textContent = movie.imdbRating;
+//
+                        //        movieNameContainer.append(movieTitle, starIcon, ratingScore);
+                    //
+                        //        const detailsContainer = document.createElement("div");
+                        //        detailsContainer.className = "movie-info-container";
+                    //
+                        //            const lengthPara = document.createElement("p");
+                        //            lengthPara.textContent = movie.Runtime;
+                    //
+                        //            const genresPara = document.createElement("p");
+                        //            genresPara.textContent = movie.Genre;
+                    //
+                        //            const addButton = document.createElement("button");
+                    //
+                        //                const addIcon = document.createElement("img");
+                        //                addIcon.src = "./media/plus.png";
+                    //
+                        //                const addPara = document.createElement("p");
+                        //                addPara.textContent = "Watchlist";
+                        //               
+                    //
+                        //            addButton.append(addIcon, addPara);
+                    //
+                    //
+                    //
+                        //        detailsContainer.append(lengthPara, genresPara, addButton);
+                    //
+                        //        const resumePara = document.createElement("p");
+                        //        resumePara.textContent = movie.Plot;
+                        //    
+                    //
+        //
+                    //
+                        //    movieContainer.append(movieNameContainer, detailsContainer, resumePara);
+//
+                        //div.append(moviePoster, movieContainer);
+                    //
+                    //resultsContainer.append(div);
                     
                     });
             });
