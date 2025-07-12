bde02ef2336150a42113a5569e0cb428fa08e93f
Deniz <auo@tuta.io>
2025-07-12 15:54:00 +0200

    fixed the bug affecting the indexing of the movie containers

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ document.addEventListener("click", function (e) {
     }
 });
 
-
-
 function handleSearchBtn(e) {
     e.preventDefault();
     resultsContainer.innerHTML = "";
@@ -27,7 +25,7 @@ function handleSearchBtn(e) {
                         //console.log(movie);
 
                         resultsContainer.innerHTML += `
-                        <div class="movie-container" id="movie-container-${i}">
+                        <div class="movie-container" id="movie-container-${movie.imdbID}">
                             <div class="grid-item poster-box">
                                 <img src="${movie.Poster}"/>
                             </div>
@@ -45,14 +43,39 @@ function handleSearchBtn(e) {
                                 </button>
                             </div>
                             <div class="grid-item theme-box">
-                                <p class="theme-para">
+                                <p class="theme-para" id="theme-para-${movie.imdbID}">
                                     ${movie.Plot}
                                 </p>
-                                <button class="read-more" id="read-more-${i}">Read More</button>
+                                <button class="read-more" id="read-more-${movie.imdbID}">Read More</button>
                             </div>
                         </div>
                         `;
-                        readMore();
+                    
+                    
+                        /*Logic to add a Read-More button if the theme is too long*/
+
+                        const themePara = document.getElementById(`theme-para-${movie.imdbID}`);
+                        //console.log(themePara);
+                        const readMoreBtn = document.getElementById(`read-more-${movie.imdbID}`);
+                        //console.log(readMoreBtn);
+
+                        console.log(
+                            "Offset: ",
+                            themePara.offsetHeight,
+                            "Scroll:",
+                            themePara.scrollHeight,
+                            "Client: ",
+                            themePara.clientHeight,
+                            "Window: ",
+                            window.getComputedStyle(themePara).lineHeight
+                        );
+                    
+                        if (themePara.scrollHeight > themePara.clientHeight + 2) {
+                            console.log("do something");
+                            
+                            readMoreBtn.style.display = "block";
+                        }
+
                     });
             });
         });
@@ -63,41 +86,31 @@ function handleSearchBtn(e) {
 }
 
 function handleReadMore(e) {
-
     const eIndex = e.target.id.split("-").pop();
     //console.log(eIndex);
     const eContainer = document.getElementById(`movie-container-${eIndex}`);
-    
+
     eContainer.classList.toggle("toggle-movie-container");
-    
-    if(e.target.textContent === "Read More") {
+
+    if (e.target.textContent === "Read More") {
         e.target.textContent = "Read Less";
     } else if (e.target.textContent === "Read Less") {
         e.target.textContent = "Read More";
     }
 }
 
-function readMore() {
-    const themePara = document.querySelectorAll(".theme-para");
-
-    themePara.forEach((para, i) => {
-        console.log(para.offsetHeight, para.scrollHeight, para.clientHeight, window.getComputedStyle(para).lineHeight);
-        const readMoreBtn = document.getElementById(`read-more-${i}`);
-
-        if (para.scrollHeight > para.offsetHeight) {
-            //console.log(i, readMoreBtn);
-            readMoreBtn.style.display = "inline-block";
-           
-        }
-    });
-}
-
-readMore();
-
-
-
-
-
-
-
-
+//function readMore() {
+//    const themePara = document.querySelectorAll(".theme-para");
+//
+//    themePara.forEach((para, i) => {
+//        console.log(para.offsetHeight, para.scrollHeight, para.clientHeight, window.getComputedStyle(para).lineHeight);
+//        const readMoreBtn = document.getElementById(`read-more-${i}`);
+//
+//        if (para.scrollHeight > para.offsetHeight) {
+//            //console.log(i, readMoreBtn);
+//            readMoreBtn.style.display = "inline-block";
+//        }
+//    });
+//}
+//
+//readMore();
